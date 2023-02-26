0f5b3668a9941666022f0c17e1f277f46bb5017c
chuahce <choongeng.chuah@gmail.com>
2023-02-26 10:21:29 +0800

    Initial Commit

---
+++ src/main/java/nus/iss/tfip/ssf/day12/_ssfworkshop/Application.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.ssf.day12._ssfworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/tfip/ssf/day12/_ssfworkshop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.ssf.day12._ssfworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
